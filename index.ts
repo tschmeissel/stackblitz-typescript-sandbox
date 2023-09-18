c8ffaf49aed6f7e312a4467ff74c669e15776104
tschmeissel <torsten.schmeissel@gmx.de>
2023-09-18 17:51:16 +0200

    use two pointer to solve

--- index.ts
+++ index.ts
@@ -1,10 +1,25 @@
-//import "./truthy_and_falsy_checking.ts"
-//import "./function_notations.ts"
-import './RomanToInt/index.ts';
+/**
+ * https://leetcode.com/explore/featured/card/fun-with-arrays/526/deleting-items-from-an-array/3247/
+ */
+//const input = [3,2,2,3];
+const input = [0,1,2,2,3,0,4,2];
+console.log(input);
 
-// console.log("this", this);
+const removeElement = (nums: number[], val: number): number => {
+  let result = nums.length, last: number, counter: number;
 
-// const aFunc = () => {
-//   console.log("this inside arrow function", this);
-// }
-// aFunc();
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      last = nums[nums.length - 1];
+      nums[nums.length - 1] = nums[i];
+      nums[i] = last;
+      result--;
+    }
+  }
+
+  console.log(nums);
+  return result;
+}
+
+//console.log(removeElement(input, 2));
+console.log(removeElement(input, 2));
